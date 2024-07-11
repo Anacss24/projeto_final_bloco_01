bdb228f22b60a0fbcaac3a5859db448dbd91e5d1
Ana Claudia <anaclaudiasantanadev@gmail.com>
2024-07-11 12:41:53 -0300

    Construção do Menu - Modas Ana

---
+++ Menu.ts
@@ -0,0 +1,76 @@
+import readlinesync = require("readline-sync");
+
+export function Menu() {
+
+    let opcao: number;
+
+    while(true) {
+
+        console.log("**************************************");
+        console.log("                                      ");
+        console.log("             Modas Ana                ");
+        console.log("                                      ");
+        console.log("**************************************");
+        console.log("                                      ");
+        console.log("    1 - Cadastrar Produto             ");
+        console.log("    2 - Listar Todos os Produtos      ");
+        console.log("    3 - Listar Produto pelo ID        ");
+        console.log("    4 - Atualizar o Produto           ");
+        console.log("    5 - Deletar 0 Produto             ");
+        console.log("    6 - Sair                          ");
+        console.log("                                      ");
+        console.log("**************************************");
+
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        switch(opcao){
+            case 1:
+                console.log("\nCadastrar Produto")
+                keyPress();
+                break;
+            case 2:
+                console.log("\nListar todos os Produtos")
+                keyPress();
+                break;
+            case 3:
+                console.log("\nListar Produto pelo ID ")
+                keyPress();
+                break;
+            case 4:
+                console.log("\nAtualizar o Produto")
+                keyPress();
+                break;
+            case 5:
+                console.log("\nDeletar o Produto")
+                keyPress();
+                break;
+
+            case 6:
+                sobre()
+                keyPress();
+                break;
+            default:
+                console.log("\nOpção inválida")
+                keyPress();
+                break;
+        }
+    }
+}
+
+function sobre(): void {
+    console.log("\n***************************************************************")
+    console.log(" Modas Ana - Refletindo Elegância em Cada Estilo")
+    console.log("\n***************************************************************")
+    console.log("Projeto Desenvolvido por: Ana Claudia")
+    console.log("github.com/https://github.com/Anacss24/projeto_final_bloco_01");
+    console.log("***************************************************************")
+    process.exit(0);
+}
+
+function keyPress(): void {
+    console.log("Pressione enter para continuar ...")
+    readlinesync.prompt();
+}
+
+Menu();
